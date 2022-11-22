6bff0ef75714b9eafee0b30778982b4ee974e741
csk200387 <102679480+csk200387@users.noreply.github.com>
2022-11-22 15:29:04 +0900

    Update Flask_ver1.py
    ver_2

--- Back/Api/Flask_ver1.py
+++ Back/Api/Flask_ver1.py
@@ -6,14 +6,17 @@ from datetime import datetime
 from pytz import timezone
 
 app = Flask(__name__)
+
 directory = "/home/saradan/mysite/anniData.json"
+#directory = "anniData.json"
+
 errorMessage = """{
 	"code": "BAD",
 	"message": "해당하는 날짜의 기념일 없음"
 }"""
 
 # 외국 서버이므로 한국 시간으로 설정
-KST = timezone('Etc/GMT-9')
+KST = timezone("Etc/GMT-9")
 
 # json load
 f = open(directory, "r", encoding="utf-8")
@@ -24,7 +27,7 @@ f.close()
 
 
 # flask page
-@app.route('/')
+@app.route("/")
 def ran() :
     dic = {}
     for i in range(1,10):
@@ -34,30 +37,30 @@ def ran() :
         d = random.randrange(10**(i-1),10**i)
         e = random.randrange(10**(i-1),10**i)
         f = random.randrange(10**(i-1),10**i)
-        dic['luckyNum'+str(i)] = [a,b,c,d,e,f]
+        dic["luckyNum"+str(i)] = [a,b,c,d,e,f]
     return dic
 
-@app.route('/hello')
+@app.route("/hello")
 def hello() :
-    return random.choice(['<body bgcolor="blue">Hi this page is hello</body>', '<body bgcolor="green">Hi this page is hello</body>', '<body bgcolor="red">Hi this page is hello</body>'])
+    return random.choice(["<body bgcolor=\"blue\">Hi this page is hello</body>", "<body bgcolor=\"green\">Hi this page is hello</body>", "<body bgcolor=\"red\">Hi this page is hello</body>"])
 
-@app.route('/annijson')
+@app.route("/annijson")
 def annijson() :
     data = json.dumps(anni_json, ensure_ascii=False)
     return data
 
-@app.route('/api')
+@app.route("/api", methods=["POST"])
 def api() :
     try :
-        mon = request.args.get("m")
-        dat = request.args.get("d")
+        mon = request.form["month"]
+        dat = request.form["date"]
         res = f"{mon}월 {dat}일"
         a = json.dumps({res:anni_json[res]}, ensure_ascii=False)
         return a
     except :
         return errorMessage
 
-@app.route('/red')
+@app.route("/red")
 def red() :
-    F = request.args.get('go')
+    F = request.args.get("go")
     return redirect(F)
